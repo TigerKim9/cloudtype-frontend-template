f4f3c9ec6a511187c96a411767ee61aee90ed943
TigerKim9 <90361096+TigerKim9@users.noreply.github.com>
2023-10-04 03:20:37 +0900

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,7 @@ const Header = () => {
             <header>
                 <nav className="flex justify-between">
                     <div className="flex items-center space-x-3 lg:pr-16 pr-6">
-                        <h2 className="font-bold text-2xl leading-6 text-gray-800">Cloudtype Example</h2>
+                        <h2 className="font-bold text-2xl leading-6 text-gray-800">개판 시즌방 마음의 편지 Version 0.1</h2>
                     </div>
                 </nav>
             </header>
